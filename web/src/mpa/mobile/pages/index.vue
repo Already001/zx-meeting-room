b7abfdf7abf4b0bbb935ab42d4f6e7c948942b0e
李权泓 <951477037@qq.com>
2026-08-24 10:46:16 +0800

    feat(web): m 移动入口与 Vant 自检页

---
+++ web/src/mpa/mobile/pages/index.vue
@@ -0,0 +1,20 @@
+<template>
+  <div>
+    <SmokeCard entry="m（iOS / 安卓 WebView）" />
+    <div class="px-16px">
+      <van-button type="primary" size="small" @click="show = true">
+        Vant 自检
+      </van-button>
+      <van-popup v-model:show="show" round position="bottom" class="p-16px">
+        <span class="text-14px text-grayDark">Vant 已生效。</span>
+      </van-popup>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import SmokeCard from "@/components/SmokeCard.vue";
+
+const show = ref(false);
+</script>
